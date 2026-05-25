6cdac6f7feea2a4e6e4ec1d8240a3eae57648830
Nova <officialnova420@gmail.com>
2026-05-25 19:20:48 +0530

    DNA complement generator and base counter now only takes up valid DNA sequences

--- DNA-base-counter/DNA-base-counter.py
+++ DNA-base-counter/DNA-base-counter.py
@@ -1,12 +1,18 @@
-inp_strand = input("Enter DNA Strand: ")
-DNA_Strand = inp_strand.upper()
-occ_dictionary = {
-    "A": DNA_Strand.count("A"),
-    "T": DNA_Strand.count("T"),
-    "G": DNA_Strand.count("G"),
-    "C": DNA_Strand.count("C")
-}
-print(f"Count of A: {occ_dictionary.get("A")} ")
-print(f"Count of T: {occ_dictionary.get("T")} ")
-print(f"Count of G: {occ_dictionary.get("G")} ")
-print(f"Count of C: {occ_dictionary.get("C")} ")
+DNA = input("Enter DNA strand: ").upper().strip()
+DNA_A = DNA.replace('A', '')
+DNA_T = DNA_A.replace('T', '')
+DNA_G = DNA_T.replace('G', '')
+DNA_C = DNA_G.replace('C', '')
+if bool(DNA_C) is False:
+    occ_dictionary = {
+        "A": DNA.count("A"),
+        "T": DNA.count("T"),
+        "G": DNA.count("G"),
+        "C": DNA.count("C")
+    }
+    print(f"Count of A: {occ_dictionary.get("A")} ")
+    print(f"Count of T: {occ_dictionary.get("T")} ")
+    print(f"Count of G: {occ_dictionary.get("G")} ")
+    print(f"Count of C: {occ_dictionary.get("C")} ")
+else:
+    print("This is not a valid DNA strand")
